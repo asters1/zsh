a94c3c33ae7e25734b8d3f7fcb0c2330b03f8cf0
asters1 <gj744559140@163.com>
2021-12-21 19:11:25 +0800

    完善配置

--- zshrc.zsh
+++ zshrc.zsh
@@ -1,3 +1,8 @@
+#安装oh-my-zsh
+if [[ ! -d $HOME/.oh-my-zsh ]];then
+	wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | sh
+	echo "source \$HOME\/.config/zsh/zshrc.zsh" > $HOME/.zshrc
+fi
 source ~/.config/zsh/init.zsh
 source ~/.config/zsh/plugins.zsh
 source $ZSH_DIR_PLUG/fzf-tab/fzf-tab.plugin.zsh
