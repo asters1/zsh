74d767ecdb8105d71b7216a9bcb4602174174fdf
asters1 <gj744559140@163.com>
2022-07-06 13:16:31 +0000

    增加Goroot

--- init.zsh
+++ init.zsh
@@ -1,5 +1,7 @@
 export ZSH=$HOME/.oh-my-zsh
 export ZSH_DIR_PLUG=$ZSH_CUSTOM/plugins
+export GOROOT=/usr/local/go
+export PATH=$PATH:$GOROOT/bin
 ZSH_THEME="robbyrussell"
 plugins=(git zsh-autosuggestions zsh-syntax-highlighting)
 source $ZSH/oh-my-zsh.sh
