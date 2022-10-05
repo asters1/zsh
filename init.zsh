2ed28e96d401c77bffa49ee721650fefdbfd16c9
asters1 <gj744559140@163.com>
2022-10-05 19:42:07 +0800

    增加jadx路径

--- init.zsh
+++ init.zsh
@@ -2,6 +2,7 @@ export ZSH=$HOME/.oh-my-zsh
 export ZSH_DIR_PLUG=$ZSH_CUSTOM/plugins
 export GOROOT=/usr/local/go
 export PATH=$PATH:$GOROOT/bin
+export PATH=$PATH:/opt/jadx/build/jadx/bin
 ZSH_THEME="robbyrussell"
 plugins=(git zsh-autosuggestions zsh-syntax-highlighting)
 source $ZSH/oh-my-zsh.sh
