780b7bb4d5b6656e0243cfbce49d99fe6144e090
asters1 <gj744559140@163.com>
2022-11-09 07:04:28 +0000

    修改java环境

--- init.zsh
+++ init.zsh
@@ -1,12 +1,10 @@
 export ZSH=$HOME/.oh-my-zsh
 export ZSH_DIR_PLUG=$ZSH_CUSTOM/plugins
 #java
-export JAVA_HOME=~/.config/nvim/java/jdk/arm/jdk-11.0.16.1
-export JDTLS_HOME=~/.config/nvim/java/jdt
-export WORKSPACE=~/javaProject
+export JAVA_HOME=/opt/jdk-11.0.17
+export PATH=$PATH:$JAVA_HOME/bin:.
+export CLASSPATH=$JAVA_HOME/lib/tools.jar:$JAVA_HOME/lib/dt.jar:.
 
-export PATH=$JAVA_HOME/bin:$PATH
-export CLASSPATH=.:$JAVA_HOME/lib/dt.jar:$JAVA_HOME/lib/tools.jar
 #go
 export GOROOT=/usr/local/go
 #jadx
